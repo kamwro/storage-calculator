05f1fc4e2531aa4d994bdb32eba51d6750b2a014
kamwro <kamilwro1998@gmail.com>
2025-12-17 01:04:12 +0100

    feat: containers DI

--- backend/src/core/tokens.ts
+++ backend/src/core/tokens.ts
@@ -5,11 +5,3 @@ export const CONTAINERS_SERVICE = Symbol('CONTAINERS_SERVICE');
 export const ITEMS_SERVICE = Symbol('ITEMS_SERVICE');
 export const CALCULATOR_SERVICE = Symbol('CALCULATOR_SERVICE');
 export const CARGO_CLIENT = Symbol('CARGO_CLIENT');
-
-export type AuthServiceToken = typeof AUTH_SERVICE;
-export type UsersServiceToken = typeof USERS_SERVICE;
-export type ItemTypesServiceToken = typeof ITEM_TYPES_SERVICE;
-export type ContainersServiceToken = typeof CONTAINERS_SERVICE;
-export type ItemsServiceToken = typeof ITEMS_SERVICE;
-export type CalculatorServiceToken = typeof CALCULATOR_SERVICE;
-export type CargoClientToken = typeof CARGO_CLIENT;
